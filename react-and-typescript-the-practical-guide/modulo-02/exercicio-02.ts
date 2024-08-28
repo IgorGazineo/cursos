9c520000e860fe79abe7bfad684c2222b87e5fca
Igor Gazineo <gaumgazineo@hotmail.com>
2024-08-28 15:00:00 -0300

    adicionando os exercícios das aulas 09, 10 e 13 do módulo 02 do curso react-and-typescript

---
+++ react-and-typescript-the-practical-guide/modulo-02/exercicio-02.ts
@@ -0,0 +1,39 @@
+/*
+
+01) Aula 09 - Configurando e usando typescript:
+
+    Descomente o alert abaixo, abra o terminal integrado e rode o comando 'npx tsc' + o nome do arquivo para invocar o compilador e gerar o arquivo javascript.
+
+    Abra o arquivo html no browser para ver o pop up do alert sendo exibido.
+
+*/
+
+// alert('Hello, TypeScript');
+
+/*
+
+02) Aula 10 - Trabalhando com tipagem: anotação de inferência de tipagem explicita:
+
+    Descomente o código das linhas 21 e 22 e faça com que a declaração da variável 'myAge' aceite a reatribuição de qualquer tipo de dados.
+
+    Descomente o código da linha 26 a 28 e faça com que a declaração da variável 'myName' não aceite a reatribuição de um tipo que não seja 'string'.
+
+*/
+
+// let myAge = '36';
+// myAge = 37;
+
+// let myName;
+// myName = 'Igor'
+// myName = 38
+
+/*
+
+03) Aula 13 - Combinando união de tipagens:
+
+    Descomente o código abaixo e faça com que a declaração da variável 'userID' aceite apenas a reatribuição do tipo number ou string.
+
+*/
+
+// let userID = "abc1";
+// userID = 123;
