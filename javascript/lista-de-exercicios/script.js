7875c0021385c74f79a39e9f29dc6d8e0f0ed94f
Igor Gazineo <gaumgazineo@hotmail.com>
2024-10-07 01:54:03 -0300

    adicionando os exercícios 01 e 02 da lista de exercícios de js

---
+++ javascript/lista-de-exercicios/script.js
@@ -0,0 +1,26 @@
+// *** Atenção ***
+// Sempre comente ou exclua o código após a conclusão de cada exercício.
+
+/*
+
+01) Crie uma const "result" que recebe um array com o terceiro e quarto item do array "arrayOfNames".
+
+    Para isso, utilize o método slice.
+
+    Exiba a const "result" no console.
+
+*/
+
+const arrayOfNames = ["Rogério", "Carlos", "Maria", "João", "José"];
+
+/*
+
+02) Crie uma const "middleName" que recebe o valor "Gazineo" da const "myName".
+
+    Para isso, utilize o método slice o método indexOf e o método lastIndexOf.
+
+    Exiba o resultado no console.
+
+*/
+
+const myName = "Igor Gazineo Rebouças";
